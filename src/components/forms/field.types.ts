68ac7fd61549da0c5bc99d03edb81c08a5bed9f2
Charles Ponti <cj@ponti.io>
2026-08-03 20:39:20 -0700

    feat: remove field and group-by

--- src/components/forms/field.types.ts
+++
@@ -1,7 +0,0 @@
-export interface FieldBaseProps {
-  error?: string | undefined;
-  helpText?: string | undefined;
-  id?: string | undefined;
-  label?: string | undefined;
-  required?: boolean | undefined;
-}
